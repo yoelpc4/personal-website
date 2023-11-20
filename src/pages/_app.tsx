e813d98fe37f84959d8fd21cf61ae840c9a34f1e
Yoel Peter Chandra <yoelpc4@gmail.com>
2023-11-20 21:14:01 +0700

    feat: add google analytics for audiences & web traffic tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Sora } from 'next/font/google'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 import TopLeftImage from '@/pages/components/TopLeftImage'
 import Header from '@/pages/components/Header'
 import Nav from '@/pages/components/Nav'
@@ -21,15 +22,18 @@ const sora = Sora({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div
-      className={`relative overflow-x-hidden bg-secondary bg-background bg-cover bg-no-repeat p-0 ${sora.variable} font-sora leading-relaxed text-white`}
-    >
-      <TopLeftImage />
-      <Header />
-      <Nav />
-      <Main>
-        <Component {...pageProps} />
-      </Main>
-    </div>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <div
+        className={`relative overflow-x-hidden bg-secondary bg-background bg-cover bg-no-repeat p-0 ${sora.variable} font-sora leading-relaxed text-white`}
+      >
+        <TopLeftImage />
+        <Header />
+        <Nav />
+        <Main>
+          <Component {...pageProps} />
+        </Main>
+      </div>
+    </>
   )
 }
